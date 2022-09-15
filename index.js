9a8c1df11f9aacb1520f40c404cfb22f1c46d701
Masaya <shimoda.masaya@cama.jp>
2022-09-16 04:46:18 +0900

    complete first-step func

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import { Client } from "@notionhq/client";
+import { WebClient } from "@slack/web-api";
 
 const notion = new Client({ auth: process.env.NOTION_KEY });
 
@@ -40,12 +41,33 @@ const checkTask = () => {
         (value.properties.date.date.end > today) &
         (value.properties.status.status.name != "Review" || "Done")
       ) {
-        nowTask.push({name:value.properties.name.title[0].plain_text,end:value.properties.date.date.end});
+        nowTask.push({
+          name: value.properties.name.title[0].plain_text,
+          end: value.properties.date.date.end,
+        });
       }
     });
-    console.log(nowTask)
+
+    nowTask.map(function (value) {
+      console.log(value.name);
+      (async () => {
+        // OAuth トークン
+        const token = process.env.SLACK_KEY;
+        // #チャンネル名 of @ユーザー名
+        const channel = "#bot_test";
+        // メッセージ
+        const text = `「${value.name}」のタスクが${value.end}に終了しちゃうよ。頑張ってね:)`;
+
+        const client = new WebClient(token);
+        const response = await client.chat.postMessage({ channel, text });
+
+        // 投稿に成功すると `ok` フィールドに `true` が入る。
+        console.log(response.ok);
+        // => true
+      })();
+    });
+    // console.log(nowTask);
   });
 };
 
 checkTask();
-
